19209c1ac8f795aae6db9ed51d183b5eb4a5c40d
arnav0405 <arnavwaghdhare@gmail.com>
2025-09-30 15:00:26 +0530

    Cell Made

--- ConvLSTMCell.py
+++ ConvLSTMCell.py
@@ -21,4 +21,18 @@ class ConvLSTMCell(nn.Module):
         combined = torch.cat([input_tensor, h_curr], dim=1)
         combined_conv = self.conv(combined)
 
-        return 
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_curr + i * g
+        h_next = o * torch.tanh(c_next)
+        return h_next, c_next
+    
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        h = torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device)
+        c = torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device)
+        return (h, c)
